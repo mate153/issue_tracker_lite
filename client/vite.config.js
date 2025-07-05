cddc67b2bfc44c6d8e26d272856c70a6872cd75b
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2025-07-05 11:59:27 +0200

    [ ADD ] initialize React client with Vite, and Tailwind CSS

---
+++ client/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.SERVER_ADDRESS,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      __SERVER_ADDRESS__: JSON.stringify(env.SERVER_ADDRESS),
+    },
+  };
+});
