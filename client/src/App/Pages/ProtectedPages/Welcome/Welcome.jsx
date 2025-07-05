cb4fce4b997582f2dafb9fde70d186b84dde7db5
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2025-07-05 12:07:49 +0200

    [ ADD ] implement Welcome component

---
+++ client/src/App/Pages/ProtectedPages/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+function Welcome() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">
+                Welcome to the Issue Tracker Lite
+            </h1>
+            <p className="mb-6">
+                Track and update your tickets, add comments, and AI even suggests titles for you.
+            </p>
+        </div>
+    );
+}
+
+export default Welcome;
